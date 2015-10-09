6bdec7147b0d83381e60ce6e821c3df7e77bf771
Admin <admin@drivers-macmini.local>
2015-10-09 15:10:19 -0700

    Initial Commit

---
+++ GitYo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GitYo
+//
+//  Created by Admin on 10/9/15.
+//  Copyright © 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
