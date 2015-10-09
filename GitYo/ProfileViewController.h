7c347dc37ae371175f64e6e4d7ecf2af25a02494
Admin <admin@Drivers-MacMini.local>
2015-10-09 16:36:07 -0700

    adding profile page

---
+++ GitYo/ProfileViewController.h
@@ -0,0 +1,13 @@
+//
+//  ProfileViewController.h
+//  GitYo
+//
+//  Created by Admin on 10/9/15.
+//  Copyright © 2015 Admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ProfileViewController : UIViewController
+
+@end
